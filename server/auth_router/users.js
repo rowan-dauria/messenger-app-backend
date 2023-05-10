6b4f9d1eefdfed9c046ffea5e1a35d5a1065a142
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2023-05-10 18:14:09 +0100

    added a users/me endpoint and changed the /users endpoint to also respond with the user email

--- server/auth_router/users.js
+++ server/auth_router/users.js
@@ -5,18 +5,29 @@ const usersRouter = express.Router();
 
 usersRouter.get('/', async (req, res) => {
   const result = await pgPool.query(
-    'SELECT id, display_name FROM users ORDER BY id ASC',
+    'SELECT id, display_name, email FROM users ORDER BY id ASC',
   );
   res.status(200).json(result.rows);
 });
 
-usersRouter.get('/:id', async (req, res) => {
+usersRouter.get('/:id', async (req, res, next) => {
   const { id } = req.params;
+  if (id === 'me') next();
+  else {
+    const result = await pgPool.query(
+      'SELECT id, display_name, email FROM users WHERE id = $1',
+      [id],
+    );
+    res.status(200).json(result.rows);
+  }
+}, async (req, res) => {
+  const { userID } = req.session.authorization;
+  console.log(userID);
   const result = await pgPool.query(
-    'SELECT id, display_name FROM users WHERE id = $1',
-    [id],
+    'SELECT id, display_name, email FROM users WHERE id = $1',
+    [userID],
   );
-  res.status(200).json(result.rows);
+  res.status(200).json(result.rows[0]);
 });
 
 module.exports = usersRouter;
